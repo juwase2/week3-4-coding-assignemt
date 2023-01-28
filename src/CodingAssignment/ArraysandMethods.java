13edb2547e391363542d6f5553a37cee8b2c9d67
Joviale Uwase <juwase2@JovialeUwase-1181.local>
2023-01-28 17:00:15 -0600

    .

--- src/CodingAssignment/ArraysandMethods.java
+++ src/CodingAssignment/ArraysandMethods.java
@@ -83,9 +83,52 @@ public class ArraysandMethods {
 	  System.out.println(createFullName("Joviale", "Uwase"));
 	  
 		
-	}
+	// 9.	Write a method that takes an array of int and returns true if the sum is 100
+	   
 	 
+	  
+	  System.out.println ( addNumbersInTheArray(ages));
+	  
+	// Question 10. Question 10: method that takes an array of double and returns the average 
+	  
+	  double [] grades = { 34, 41, 37, 50, 39, 49 }; //new array of doubles created.
+	  
+	  System.out.println(createAverageGrades(grades)) ;
+	  
+	  
+	// Question 11. Write a method to compare two arrays
+	  
+	  double [] counts = { 12, 67, 22, 24, 90, 21 }; 
+	  
+	  System.out.println(createAverageCounts(counts)) ;
+			  
+	  System.out.println( FindWhichAverageIsBigger(grades,counts));
+	  
+	// Question 12: method called willBuyDrink that takes a boolean isHotOutside, and a double moneyInPocket
+	  
+	  boolean isHotOutside = true;
+	  double moneyInPocket = 19.5; 
+	  
+	  System.out.println(willBuyDrink(isHotOutside, moneyInPocket));
+
+	// 13.	Create a method of your own that solves a problem. I created a method to tell me if it is extremely cold or not.
+	  
+	  int temperatureinF = -10;
+	  boolean isColdOutside = true; 
+	  
+	  System.out.println(weatherNotification(temperatureinF,isColdOutside));
+	  
+	  
+	  
+	  
+	}
+	
+	
+	
+	
+	//Question 7 Write a method
 	private static String createWordNtimes( String word, int n) {
+		
 		String answer = "";
 		for (int i = 0; i < n; i++) {
 			answer+= word;
@@ -98,10 +141,110 @@ public class ArraysandMethods {
 	private static String createFullName( String FirstName, String LastName) {
 		
 		return FirstName + " " + LastName;
-	}
-}	
+	   }
+	
+	//9.	Write a method that takes an array of int and returns true if the sum is 100
+	
+	
+      private static boolean addNumbersInTheArray( int numbers[]) {
+		
+    	  int sum = 0;
+    	  
+    	  for (int number : numbers) {
+    		  
+    		  sum += number;
+    	  }
+    	  
+    	  if (sum > 100) {
+    		  return true;
+    	  } else {
+    		  return false; 
+    	  }
+
+   }
+      //Question 10: method that takes an array of double and returns the average 
+      private static double createAverageGrades( double[] grades) {
+  		
+    	  int sum = 0;
+    	  
+    	  for (double grade : grades) {
+    		  
+    		  sum += grade;
+    	  }
+    	  
+    	  return sum / grades.length; 
+ }
+      
+   // Question 11. Write a method to compare two arrays
+      
+      
+       private static double createAverageCounts( double[] counts) {
+  		
+    	  int sum = 0;
+    	  
+          for ( double count : counts) {
+        	  sum += count;
+        	  
+          }
+        	 return sum / counts.length;
+         }
+         
+       
+       private static boolean FindWhichAverageIsBigger( double[] x , double [] y) {
+     		
+     	  double average1 = createAverageGrades(x);
+     	  double average2 = createAverageCounts(y);
+     	  
+     	  if (average1 > average2) {
+     		  return true;
+     	  }else {
+     		  return false;
+     	  }
+     	  }
+       // Question 12: method called willBuyDrink that takes a boolean isHotOutside, and a double moneyInPocket
+ 
+       private static boolean willBuyDrink( boolean x , double y) {
+
+       if ( (x = true) &&  (y > 10.5) ) {
+        	
+          return true;
+        } else {
+        	
+        	return false;
+        }
+ }
+
+      // Question 13: Create a method of your own that solves a problem. 
+       
+       
+       private static String weatherNotification (int x , boolean y) {
+    	   
+    	   String weather = "it is extremely-cold"; 
+    	   String weather1 = "it's cold outside"; 
+    	  
+           if ( (x < 0 && x > -32) && ( y = true) ) {
+            	 return weather;
+            
+           } else {
+        	    return weather1;
+           }
+            
+     
+}
+     
+}
+    
+ 
+      
+
+		
+      
+	
+
+
      
 	
+
 	 
       
           
